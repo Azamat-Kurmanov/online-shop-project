48f4be15d46cb92377a840b5aaab580999397063
azama <a.kurmanov@csi.kz>
2023-01-28 04:17:18 +0600

    Lesson-1

---
+++ src/main/java/ru/gb/spring/shopprj/ShopPrjApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.spring.shopprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopPrjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopPrjApplication.class, args);
+    }
+
+}
