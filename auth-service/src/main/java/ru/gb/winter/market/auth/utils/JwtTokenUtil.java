e9b8d37d70ea2fb5b70f84eed22d95cc6d2f75e7
azama <a.kurmanov@csi.kz>
2023-03-05 02:22:04 +0600

    Lesson-6

--- auth-service/src/main/java/ru/gb/winter/market/auth/utils/JwtTokenUtil.java
+++ auth-service/src/main/java/ru/gb/winter/market/auth/utils/JwtTokenUtil.java
@@ -1,17 +1,16 @@
-package ru.gb.winter.market.core.utils;
+package ru.gb.winter.market.auth.utils;
 
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Component
@@ -19,16 +18,16 @@ public class JwtTokenUtil {
     @Value("${jwt.secret}")
     private String secret;
 
-    @Value(("${jwt.lifetime}"))
+    @Value("${jwt.lifetime}")
     private Integer jwtLifetime;
 
-    //----Парсирование данных пользователя и генерация токена в закодированном виде
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         List<String> rolesList = userDetails.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
                 .collect(Collectors.toList());
         claims.put("roles", rolesList);
+
         Date issuedDate = new Date();
         Date expiredDate = new Date(issuedDate.getTime() + jwtLifetime);
         return Jwts.builder()
@@ -39,23 +38,4 @@ public class JwtTokenUtil {
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
-
-    //----Обратное парсирование данных пользователя и получение данных из токена в нормальном виде
-    private Claims getAllClaimsFromToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(secret)
-                .parseClaimsJws(token)
-                .getBody();
-    }
-
-    //----Получает все сведения юзера
-    public String getUsernameFromToken(String token) {
-        return getAllClaimsFromToken(token).getSubject();
-    }
-
-    //----Получает список всех ролей авторизованного юзера
-    public List<String> getRoles(String token){
-        return getAllClaimsFromToken(token).get("roles", List.class);
-    }
-
 }
